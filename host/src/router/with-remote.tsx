8073b4fea2099037e9988e2f46820624c5cee5fe
WillowRyu <axwell@medibloc.org>
2024-08-02 02:20:09 +0900

    fix-lazy

--- host/src/router/with-remote.tsx
+++ host/src/router/with-remote.tsx
@@ -1,13 +1,20 @@
-import { Hello } from "app_02/Hello";
+import { loadRemote } from "@module-federation/runtime";
+import { lazy, Suspense } from "react";
 import { useNavigate } from "react-router-dom";
+
+const Hello = lazy(() =>
+  loadRemote("app_02/Hello").then((module) => ({ default: module.Hello }))
+);
 
 export function WithRemote() {
   const navigate = useNavigate();
 
   return (
     <div>
-      With Remote2
-      <Hello name={"222"} />
+      With Remote
+      <Suspense>
+        <Hello name={"222"} />
+      </Suspense>
       <div style={{ padding: "20px" }}></div>
       <button
         onClick={() => {
